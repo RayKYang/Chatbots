a6c59cbd9a304f1575aaa757321f846cf135e98d
Ray K Yang <ray.cn.us@gmail.com>
2018-08-24 22:10:25 -0700

    update

--- ML_Chatbots.py
+++ ML_Chatbots.py
@@ -18,7 +18,7 @@ def send_message(message):
 # Send a message to the bot
 send_message("hello")
 
-# 1.b Small talk
+## 1.b(1) Small talk
 responses = {
     "what's your name?": "my name is EchoBot",
     "what's the weather today?": "it's sunny!"
@@ -30,9 +30,39 @@ def respond(message):
 
 respond("what's your name?")
 
+# 1.b(2) including variables
+responses = {
+    "what's today's weather?": "it's {} today"
+}
 
+weather_today = "cloudy"
 
+def respond(message):
+    if message in responses:
+        return responses[message].format(weather_today)
 
+# 1.b(3) choosing responses:
+responses = {
+    "what's your name?": [
+        "my name is EchoBot",
+        "they call me EchoBot",
+        "the name's Bot, Echo Bot"
+    ]
+}
+import random
+def respond(message):
+    if message in responses:
+        return random.choice(responses[message])
+
+respond("what's your name?")
+
+# 1.b(4) Asking questions
+responses = ["tell me more!", "why do you think that?"]
+import random
+def respond(message):
+    return random.choice(responses)
+
+respond("I think you're really great")
 
 import time
 time.sleep(0.5)
