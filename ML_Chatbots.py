1a0fd1cf1a1e6ba9797780f608102c40dcd33588
Ray K Yang <ray.cn.us@gmail.com>
2018-08-26 23:44:08 -0700

    update

--- ML_Chatbots.py
+++ ML_Chatbots.py
@@ -23,7 +23,6 @@ responses = {
     "what's your name?": "my name is EchoBot",
     "what's the weather today?": "it's sunny!"
 }
-
 def respond(message):
     if message in responses:
         return responses[message]
@@ -34,14 +33,32 @@ respond("what's your name?")
 responses = {
     "what's today's weather?": "it's {} today"
 }
-
 weather_today = "cloudy"
-
 def respond(message):
     if message in responses:
         return responses[message].format(weather_today)
+# add default
+# Define variables
+name = "Greg"
+weather = "cloudy"
+# Define a dictionary with the predefined responses
+responses = {
+  "what's your name?": "my name is {0}".format(name),
+  "what's today's weather?": "the weather is {0}".format(weather),
+  "default": "default message"
+}
+# Return the matching response if there is one, default otherwise
+def respond(message):
+    # Check if the message is in the responses
+    if message in responses:
+        # Return the matching message
+        bot_message = responses[message]
+    else:
+        # Return the "default" message
+        bot_message = responses["default"]
+    return bot_message
 
-# 1.b(3) choosing responses:
+# 1.b(3) choosing responses randomly:
 responses = {
     "what's your name?": [
         "my name is EchoBot",
@@ -56,13 +73,60 @@ def respond(message):
 
 respond("what's your name?")
 
+# a fuller example
+import random
+name = "Greg"
+weather = "cloudy"
+# Define a dictionary containing a list of responses for each message
+responses = {
+  "what's your name?": [
+      "my name is {0}".format(name),
+      "they call me {0}".format(name),
+      "I go by {0}".format(name)
+   ],
+  "what's today's weather?": [
+      "the weather is {0}".format(weather),
+      "it's {0} today".format(weather)
+    ],
+  "default": ["default message"]
+}
+# Use random.choice() to choose a matching response
+def respond(message):
+    # Check if the message is in the responses
+    if message in responses:
+        # Return a random matching response
+        bot_message = random.choice(responses[message])
+    else:
+        # Return a random "default" response
+        bot_message = random.choice(responses["default"])
+    return bot_message
+
 # 1.b(4) Asking questions
 responses = ["tell me more!", "why do you think that?"]
 import random
 def respond(message):
     return random.choice(responses)
-
 respond("I think you're really great")
 
+# a fuller example
+responses = {'question': ["I don't know :(", 'you tell me!'],
+ 'statement': ['tell me more!',
+  'why do you think that?',
+  'how long have you felt this way?',
+  'I find that extremely interesting',
+  'can you back that up?',
+  'oh wow!',
+  ':)']}
+import random
+def respond(message):
+    # Check for a question mark
+    if message.endswith("?"):
+        # Return a random question
+        return random.choice(responses["question"])
+    # Return a random statement
+    return random.choice(responses["statement"])
+
+
+# add delay
 import time
 time.sleep(0.5)
