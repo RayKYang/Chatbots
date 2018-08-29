efec412a6c969045616d41100e9d43e88c780fad
R.K. Yang <32747361+RayKYang@users.noreply.github.com>
2018-08-29 16:04:42 -0700

    Update ML_Chatbots.py

--- ML_Chatbots.py
+++ ML_Chatbots.py
@@ -126,6 +126,73 @@ def respond(message):
     # Return a random statement
     return random.choice(responses["statement"])
 
+# text munging with regular expressions
+rules = {'I want (.*)': ['What would it mean if you got {0}',
+  'Why do you want {0}',
+  "What's stopping you from getting {0}"],
+ 'do you remember (.*)': ['Did you think I would forget {0}',
+  "Why haven't you been able to forget {0}",
+  'What about {0}',
+  'Yes .. and?'],
+ 'do you think (.*)': ['if {0}? Absolutely.', 'No chance'],
+ 'if (.*)': ["Do you really think it's likely that {0}",
+  'Do you wish that {0}',
+  'What do you think about {0}',
+  'Really--if {0}']}
+import re # regular expression 
+def match_rule(rules, message):
+    response, phrase = "default", None
+    
+    # Iterate over the rules dictionary
+    for pattern, responses in rules.items():
+        # Create a match object
+        match = re.search(pattern, message)
+        if match is not None:
+            # Choose a random response
+            response = random.choice(responses)
+            if '{0}' in response:
+                phrase = match.group(1)
+    # Return the response and phrase
+    return response, phrase
+
+# Test match_rule
+print(match_rule(rules, "do you remember your last birthday"))
+
+# Define replace_pronouns()
+def replace_pronouns(message):
+    message = message.lower()
+    if 'me' in message:
+        # Replace 'me' with 'you'
+        return re.sub("me", "you", message)
+    if 'my' in message:
+        return re.sub("my", "your", message)
+    if 'your' in message:
+        return re.sub("your", "my", message)
+    if 'you' in message:
+        return re.sub("you", "me", message)
+    return message
+
+print(replace_pronouns("my last birthday"))
+print(replace_pronouns("when you went to Florida"))
+print(replace_pronouns("I had my own castle"))
+
+# response by integrating match_rule(), send_message(), replace_pronouns()
+# Define respond()
+def respond(message):
+    # Call match_rule
+    response, phrase = match_rule(rules, message)
+    if '{0}' in response:
+        # Replace the pronouns in the phrase
+        phrase = replace_pronouns(phrase)
+        # Include the phrase in the response
+        response = response.format(phrase)
+    return response
+
+# Send the messages
+send_message("do you remember your last birthday")
+send_message("do you think humans should be worried about AI")
+send_message("I want a robot friend")
+send_message("what if you could be anything you wanted")
 
 # add delay
 import time
